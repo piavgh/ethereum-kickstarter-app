6e63edf042c11d6a761c8f7d94007a19f55a8b3c
Hoang Trinh <hoang.trinhj@gmail.com>
2018-10-03 16:32:20 +0700

    Minor fix

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
 import web3 from './web3';
 import campaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(campaignFactory.interface),
-    process.env.ADDRESS
+    '0x60F1b8cf318B18bC5A1215d25E246d29bC34C780'
 );
 
 export default instance;
