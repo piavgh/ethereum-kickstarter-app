bd9b88a3a53883ab481f70e079606b73aeaf55a1
Hoang Trinh <hoang.trinhj@gmail.com>
2018-10-03 16:32:01 +0700

    Refactor web3 instance to work with SSR

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,17 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // We are in the browser and Metamask is running
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // We are on the server *OR* the user is not running Metamask
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/ebaf1785cc1b4f319e0ff07f26cadae8'
+    );
+
+    web3 = new Web3(provider);
+}
 
 export default web3;
