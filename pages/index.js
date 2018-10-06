250c5f82d5323d57c81906e7c7fc469dc8b0abe3
Hoang Trinh <hoang.trinhj@gmail.com>
2018-10-06 22:08:46 +0700

    Add campaign detail

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Card, Button } from 'semantic-ui-react';
 
 import factory from '../ethereum/factory';
 import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
@@ -15,7 +16,11 @@ class CampaignIndex extends Component {
         const items = this.props.campaigns.map(address => {
             return {
                 header: address,
-                description: <a>View Campaign</a>,
+                description: (
+                    <Link route={`/campaigns/${address}`}>
+                        <a>View Campaign</a>
+                    </Link>
+                ),
                 fluid: true
             };
         });
@@ -28,12 +33,17 @@ class CampaignIndex extends Component {
             <Layout>
                 <div>
                     <h3>Open Campaigns</h3>
-                    <Button
-                        floated="right"
-                        content="Create Campaign"
-                        icon="add"
-                        primary
-                    />
+                    <Link route="/campaigns/new">
+                        <a>
+                            <Button
+                                floated="right"
+                                content="Create Campaign"
+                                icon="add"
+                                primary
+                            />
+                        </a>
+                    </Link>
+
                     {this.renderCampaigns()}
                 </div>
             </Layout>
