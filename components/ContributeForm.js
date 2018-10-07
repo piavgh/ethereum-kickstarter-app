39cef23037b8e5eebf7b5095703ecc91f12bf76f
Hoang Trinh <hoang.trinhj@gmail.com>
2018-10-07 16:47:48 +0700

    Handle contribute action

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,15 +1,79 @@
 import React, { Component } from 'react';
 import { Form, Input, Button, Message } from 'semantic-ui-react';
+import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
+    state = {
+        value: '',
+        errorMessage: '',
+        loading: false
+    };
+
+    handleInputChange = event => {
+        this.setState({
+            value: event.target.value
+        });
+    };
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        this.setState({
+            errorMessage: '',
+            loading: true
+        });
+
+        const campaign = Campaign(this.props.address);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+
+            Router.replaceRoute(`/campaigns/${this.props.address}`);
+        } catch (err) {
+            this.setState({
+                errorMessage: err.message
+            });
+        }
+
+        this.setState({
+            value: '',
+            loading: false
+        });
+    };
+
     render() {
         return (
-            <Form>
+            <Form
+                onSubmit={this.handleSubmit}
+                error={!!this.state.errorMessage}
+            >
                 <Form.Field>
                     <label>Amount To Contribute</label>
-                    <Input label="ether" labelPosition="right" />
+                    <Input
+                        value={this.state.value}
+                        label="ether"
+                        labelPosition="right"
+                        onChange={this.handleInputChange}
+                    />
                 </Form.Field>
-                <Button>Contribute!</Button>
+                <Message
+                    error
+                    header="Oops!"
+                    content={this.state.errorMessage}
+                />
+                <Button
+                    loading={this.state.loading}
+                    disabled={this.state.loading}
+                    primary
+                >
+                    Contribute!
+                </Button>
             </Form>
         );
     }
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Grid } from 'semantic-ui-react';
 
 import factory from '../../ethereum/factory';
 import Layout from '../../components/Layout';
@@ -14,6 +14,7 @@ class CampaignShow extends Component {
         const summary = await campaign.methods.getSummary().call();
 
         return {
+            address: props.query.address,
             minimumContribution: summary[0],
             balance: summary[1],
             requestsCount: summary[2],
@@ -71,8 +72,12 @@ class CampaignShow extends Component {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
-                {this.renderCards()}
-                <ContributeForm />
+                <Grid>
+                    <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+                    <Grid.Column width={6}>
+                        <ContributeForm address={this.props.address} />
+                    </Grid.Column>
+                </Grid>
             </Layout>
         );
     }
