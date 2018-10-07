9a9b1ae3edb9a05b3ced57d08c53e6eaa679bccc
Hoang Trinh <hoang.trinhj@gmail.com>
2018-10-07 11:17:24 +0700

    Redeploy contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import campaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(campaignFactory.interface),
-    '0x60F1b8cf318B18bC5A1215d25E246d29bC34C780'
+    '0x5C4163C75842b55C5fD0360d12E6df2a56fBc5C5'
 );
 
 export default instance;
